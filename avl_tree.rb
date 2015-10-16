a5483af20f420c5de7e41b8bfd3c7d86e86c8807
Ivan McCarter <ivanmccarter@gmail.com>
2015-10-16 16:17:54 -0700

    Update tree method, todo: rotations:

--- avl_tree.rb
+++ avl_tree.rb
@@ -39,3 +39,92 @@ class AVLNode
     end
   end
 end
+
+class AVLTree
+  def initialize
+    @root = nil
+  end
+
+  def empty?
+    @root.nil?
+  end
+
+  def include?(val)
+    vertex, parent = find(val)
+    !!vertex
+  end
+
+  def insert(val)
+    if self.empty?
+      @root = AVLNode.new(val)
+      return true
+    end
+
+    vertex, parent = find(val)
+    return false if vertex
+
+    vertex = AVLNode.new(val)
+    if val < parent.val
+      parent.left = vertex
+    else
+      parent.right = vertex
+    end
+    vertex.parent = parent
+
+    update(parent)
+
+    true
+  end
+
+  def find(val)
+    vertex, parent = @root, nil
+
+    until vertex.nil?
+      break if vertex.val == val
+
+      parent = vertex
+      if val < vertex.val
+        vertex = vertex.left
+      else
+        vertex = vertex.right
+      end
+    end
+
+    [vertex, parent]
+  end
+
+  def update(vertex)
+    return if vertex.nil?
+
+    if vertex.balance == -2
+      if vertex.left.balance == 1
+        left_rotate!(vertex.left) #TODO
+      end
+
+      right_rotate!(vertex) #TODO
+
+    elsif vertex.balance == 2
+      if vertex.right.balance == -1
+        right_rotate!(vertex.right) #TODO
+      end
+
+      left_rotate!(vertex) #TODO
+
+    elsif vertex.balance.abs < 2
+      # Do nothing, already balanced
+    end
+
+    vertex.recalculate_depth!
+    update(vertex.parent)
+  end
+
+  def left_rotate!(parent)
+    #TODO
+  end
+
+  def right_rotate!(parent)
+    #TODO
+  end
+
+end
+
